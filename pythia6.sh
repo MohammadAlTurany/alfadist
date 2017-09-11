be6e1d2f843e7a2cea3ed383616025b29814fcca
Florian Uhlig <f.uhlig@gsi.de>
2017-09-11 15:35:18 +0200

    Add possibility to compile Pythia6 with a non system compiler.
    Create symbolic link to fix an error when running the FairRoot test macros.

--- pythia6.sh
+++ pythia6.sh
@@ -10,13 +10,18 @@ build_requires:
 ---
 #!/bin/sh
 
-cmake ${SOURCEDIR}                           \
-      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-      -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}
+cmake                                                                 \
+      ${C_COMPILER:+-DCMAKE_C_COMPILER=$C_COMPILER}                   \
+      ${CXX_COMPILER:+-DCMAKE_CXX_COMPILER=$CXX_COMPILER}             \
+      ${Fortran_COMPILER:+-DCMAKE_Fortran_COMPILER=$Fortran_COMPILER} \
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                          \
+      -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}                           \
+      ${SOURCEDIR}
 
 make ${JOBS+-j$JOBS}
 make install
-tar -c lib include | tar -x -C $INSTALLROOT 
+ln -s libpythia6.so $INSTALLROOT/lib/libPythia6.so
+tar -c lib include | tar -x -C $INSTALLROOT
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
